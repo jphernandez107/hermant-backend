771cc921e3d5b5025fc97e07a3ed5a233707114c
Juan Hernandez <jphernandez107@gmail.com>
2023-07-12 16:15:56 -0300

    Allowing all origins to access the backend.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 const routes = require('./routes/routes')
-const allowedOrigins = ['http://localhost:3000', 'https://master.d9oqcgcqxj3zw.amplifyapp.com', 'https://dev.d9oqcgcqxj3zw.amplifyapp.com'];
+const allowedOrigins = ['http://localhost:3000', 'https://master.d9oqcgcqxj3zw.amplifyapp.com', 'https://dev.d9oqcgcqxj3zw.amplifyapp.com', '*'];
 
 app.use(express.json())
 
@@ -17,7 +17,7 @@ app.use(cors({
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin. jeje';
+                'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
